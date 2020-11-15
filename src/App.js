15535421fe44f51e1ff64f8bfe1a20d191eeb8b7
David Loder <davidloder92@gmail.com>
2020-11-15 08:20:39 -0500

    google tag patch 4

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Portfolio from './Components/Portfolio';
 
 export const initGA = () => {
   console.log('GA initialized');
-  ReactGA.initialize('UA-R59JW3JQH-4')
+  ReactGA.initialize('G-R59JW3JQH4')
 }
 export const logPageView = () => {
   console.log('PageLog init')

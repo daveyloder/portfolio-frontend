962bd6afbf77b23f720b3a8094c6dad514ebc370
David Loder <davidloder92@gmail.com>
2020-11-17 20:37:33 -0500

    Add ga tag to Head.js

--- src/Components/Head.js
+++ src/Components/Head.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
 
 export default class Head extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            gtag:"G-V70HNPTL6Z"
+        };
+    };
+    configureGTag(){
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments) 
+        gtag('js', new Date());
+    
+        gtag('config', this.gtag);
+        
+    };
+
     render() {
         if (this.props.data) {
             var title = this.props.data.name;
@@ -11,8 +26,11 @@ export default class Head extends Component {
             <div>
                 <Helmet>
                     <title>{title} {occupation}</title>
+                    {/* Global site tag (gtag.js) - Google Analytics */}
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-V70HNPTL6Z"></script>
+                    <script>{configureGTag()}</script>
                 </Helmet>
             </div>
         )
     }
-}
+};

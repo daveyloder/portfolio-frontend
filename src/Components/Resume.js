0f141364070a931784b2caf4f1b3a07643ebb63d
David Loder <davidloder92@gmail.com>
2020-11-20 10:08:15 -0500

    remove GPA from Resume.js and removed percentage on skills graphs

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -7,7 +7,7 @@ class Resume extends Component {
       var skillmessage = this.props.data.skillmessage;
       var education = this.props.data.education.map(function(education){
         return <div key={education.school}><h3>{education.school}</h3>
-        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em><span>&bull;</span><em>GPA: {education.GPA}</em></p>
+        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
         
         <p>{education.description}</p></div>
       })
@@ -19,7 +19,7 @@ class Resume extends Component {
       })
       var skills = this.props.data.skills.map(function(skills){
         var className = 'bar-expand '+skills.name.toLowerCase();
-        return <li key={skills.name}><span style={{width:skills.level}}className={className}>{skills.level}</span><em>{skills.name}</em></li>
+        return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
       })
     }
 

d27397a08770df35bb5ad418976d447ea86d72b4
daveyloder <davidloder92@gmail.com>
2023-09-10 15:06:11 -0400

    Contact info update

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,53 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class About extends Component {
   render() {
-
-    if(this.props.data){
+    if (this.props.data) {
       var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
+      var profilepic = "images/" + this.props.data.image;
       var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
       var email = this.props.data.email;
+      var mailToString = `mailto:${email}?subject=Contact from davidloder.com`;
       var resumeDownload = this.props.data.resumedownload;
     }
 
     return (
       <section id="about">
-      <div className="row">
-         <div className="three columns" id="profile-container">
-            <img className="profile-pic"  src={profilepic} alt="David Loder Profile Pic" />
-         </div>
-         <div className="nine columns main-col">
+        <div className="row">
+          <div className="three columns" id="profile-container">
+            <img
+              className="profile-pic"
+              src={profilepic}
+              alt="David Loder Profile Pic"
+            />
+          </div>
+          <div className="nine columns main-col">
             <h2>About Me</h2>
 
             <p>{bio}</p>
             <div className="row">
-               <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-						   <span>{name}</span><br />
-						   <span>{street}<br />
-						         {city} {state}, {zip}
-                   </span><br />
-						   <span><a href="tel:16094021410">{phone}</a></span><br />
-                     <span><a href="mailto:davidloder92@gmail.com">{email}</a></span>
-					   </p>
-               </div>
-               <div className="columns download">
-                  <p>
-                     <a href={resumeDownload} className="button" download><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
-               </div>
+              <div className="columns contact-details">
+                <h2>Contact Details</h2>
+                <p className="address">
+                  <span>
+                    <a href={mailToString}>{email}</a>
+                  </span>
+                </p>
+              </div>
+              <div className="columns download">
+                <p>
+                  <a href={resumeDownload} className="button" download>
+                    <i className="fa fa-download"></i>Download Resume
+                  </a>
+                </p>
+              </div>
             </div>
-         </div>
-      </div>
-
-   </section>
+          </div>
+        </div>
+      </section>
     );
   }
 }
